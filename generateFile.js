b34b83b015ac66e8e5ebdacc25a1068da02f0f5b
Firuz <firuz.akhmadov@gmail.com>
2021-08-24 03:32:02 +0800

    try

--- generateFile.js
+++ generateFile.js
@@ -1,19 +1,26 @@
 const fs = require("fs");
 const path = require("path");
 const { v4: uuid } = require("uuid");
-
-const dirCodes = path.join(__dirname, "codes");
-
-if (!fs.existsSync(dirCodes)) {
-  fs.mkdirSync(dirCodes, { recursive: true });
-}
+const aws = require('aws-sdk');
+aws.config.region = 'ap-southeast-1';
+const S3_BUCKET = process.env.S3_BUCKET;
 
 const generateFile = async (format, content) => {
   const jobId = uuid();
-  const filename = `${jobId}.${format}`;
-  const filepath = path.join(dirCodes, filename);
-  await fs.writeFileSync(filepath, content);
-  return filepath;
+  const s3 = new aws.S3();
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: `${jobId}`,
+    Expires: 60,
+    ContentType: `${format}`,
+    ACL: 'public-read'
+  };
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if(err){
+      console.log(err);
+    }
+    return `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+  });
 };
 
 module.exports = {
