bac68c32129ea372dcbe508068584222f021e144
Firuz <firuz.akhmadov@gmail.com>
2021-08-24 00:55:50 +0800

    mangodb

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 mongoose.connect(
-  "mongodb://localhost/compilerdb",
+  "mongodb+srv://kidocode:kidocode@ktrialinfo.kbp1y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
