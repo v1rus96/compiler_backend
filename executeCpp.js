9ea4e5f1c7b9f894228cbd8ad59605c10650820d
Firuz <firuz.akhmadov@gmail.com>
2021-08-24 16:07:11 +0800

    sd

--- executeCpp.js
+++ executeCpp.js
@@ -11,7 +11,7 @@ if (!fs.existsSync(outputPath)) {
 const executeCpp = (filepath) => {
   const jobId = path.basename(filepath).split(".")[0];
   const outPath = path.join(outputPath, `${jobId}.out`);
-
+  console.log(outPath)
   return new Promise((resolve, reject) => {
     exec(
       `g++ ${filepath} -o ${outPath} && cd ${outputPath} && ./${jobId}.out`,
--- executePy.js
+++ executePy.js
@@ -3,7 +3,7 @@ const { exec } = require("child_process");
 const executePy = (filepath) => {
   return new Promise((resolve, reject) => {
     exec(
-      `aws s3 cp ${filepath} - | python`,
+      `python ${filepath}`,
       (error, stdout, stderr) => {
         error && reject({ error, stderr });
         stderr && reject(stderr);
